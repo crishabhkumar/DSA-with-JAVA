b709034ecf0965f47364b1f6c802538ee2c78b4d
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-09-10 23:55:35 +0530

    Add files via upload

---
+++ Conditions And loop/Loops.java
@@ -0,0 +1,68 @@
+package com.rishabh;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Q.1 print from 1 to 5
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+//
+        //for upper task we use loops
+
+        //FOR LOOP Syntax
+        /*
+            for(intialization;condition;increment or decrement){
+                //body
+                }
+         */
+//        for(int i = 1; i <= 5; i++){
+//            System.out.println(i);
+//        }
+//
+//        //Q2. print number from 1 to n
+//        int n = input.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            System.out.println("Hello World!");
+//        }
+//
+
+        /*
+            WHIlE LOOP syntaz:
+            while(condition){
+                    //body
+                 }
+         */
+//        int num = 1;
+//        while(num <= 5){
+//            System.out.println(num);
+//            num++;
+//        }
+//
+        //we use while when we dont know how many times loop will be executed
+        /*
+            DO WHILe
+            do{
+                //body
+                }while(condition)
+         */
+
+
+        int n= 1;
+        do{
+            System.out.println("Hello World!");
+            //n++;
+        }while (n != 1);
+
+
+
+
+
+
+
+    }
+}
