20bd9c88dcb50393c7c8a63fbe010237bc9ecda4
Rishabh Kumar <70275515+crishabhkumar@users.noreply.github.com>
2021-09-28 14:27:06 +0530

    Create Bubble Sort.java

---
+++ Sorting/Bubble Sort.java
@@ -0,0 +1,32 @@
+package com.rishabh.bubble;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,-2,3,-4,5,0};
+        BubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void BubbleSort(int[] arr){
+        boolean swapped;
+        //run the loop for n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            //run internal loop
+            //for less than n-i
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                //swap if condition satisfy
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            //if swapped did not work that means array is soted then just break it.
+            if(swapped == false){
+                break;
+            }
+        }
+    }
+    
+}
