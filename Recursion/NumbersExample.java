8fe966a8030d496fd378a83f58fdf911b379ad1e
Rishabh Kumar <crishabhkumar@gmail.com>
2021-10-17 18:15:36 +0530

    Add files via upload

---
+++ Recursion/NumbersExample.java
@@ -0,0 +1,34 @@
+package com.rishabh;
+//Example for function calls
+public class NumbersExample {
+    public static void main(String[] args) {
+        //waf that takes in a number and prints it
+        //print first 5 numbers
+        print1(1);
+    }
+
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+
+    static void print2(int n){
+        System.out.println(n);
+        print3(3);
+    }
+
+    static void print3(int n){
+        System.out.println(n);
+        print4(4);
+    }
+
+    static void print4(int n){
+        System.out.println(n);
+        print5(5);
+    }
+
+    static void print5(int n){
+        System.out.println(n);
+    }
+
+}
